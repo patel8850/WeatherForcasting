f5618e190ef33d72fcafea887f37940f93df736c
Rohit Patel <patelrohit8850@gmail.com>
2025-11-11 19:53:39 +0530

    fixed error and ready to redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { WeatherProvider } from './Context/Weather.jsx'
+import { WeatherProvider } from './Context/Weather.jsx' 
+import { HashRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRouter>
   <WeatherProvider>
     <App />
     </WeatherProvider>
+    </HashRouter>
   </StrictMode>,
 
 )
